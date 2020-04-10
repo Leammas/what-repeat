a6866d3c7fce268e12de7ea7d281dd981bd514ff
Vlasdimir Samoylov <leammas@gmail.com>
2020-04-10 16:03:27 +0300

    wip

--- src/main/scala/com/github/leammas/commutativity/ByDesignSequentialProcess.scala
+++ src/main/scala/com/github/leammas/commutativity/ByDesignSequentialProcess.scala
@@ -1,27 +1,41 @@
 package com.github.leammas.commutativity
 
+import cats.MonadError
 import cats.data.NonEmptyList
 import cats.effect.Sync
 import cats.effect.concurrent.Ref
-import com.github.leammas.DemoCase
 import cats.implicits._
+import com.github.leammas.DemoCase
 
 object ByDesignSequentialProcess {
 
-  def run[F[_]](repo: PaidItemCounter[F]): OrderEvent => F[CountResult] =
-    e => repo.incrementForEvent(e)
+  type MonadThrowable[F[_]] = MonadError[F, Throwable]
+
+  def run[F[_]: MonadThrowable](
+      repo: PaidItemCounter[F]): OrderEvent => F[Unit] =
+    e =>
+      repo.incrementForEvent(e).flatMap {
+        case CountResult.ItemCountSaved => ().pure[F]
+        case CountResult.OrderNotFound =>
+          (new RuntimeException("OrderNotFound")).raiseError[F, Unit]
+    }
 
   def demo[F[_]](implicit F: Sync[F]): DemoCase[F] = new DemoCase[F] {
     def demonstrate: F[Unit] =
       for {
         _ <- F.delay(println("ByDesignSequentialProcess"))
-        orderItemCountRef <- Ref.of[F, Map[String, Int]](Map.empty)
-        totalRef <- Ref.of[F, Int](0)
-        itemCounter = new InMemoryItemCounter[F](orderItemCountRef, totalRef)
+        orderItemCountRef1 <- Ref.of[F, Map[String, Int]](Map.empty)
+        totalRef1 <- Ref.of[F, Int](0)
+        orderItemCountRef2 <- Ref.of[F, Map[String, Int]](Map.empty)
+        totalRef2 <- Ref.of[F, Int](0)
+        itemCounter1 = new InMemoryItemCounter[F](orderItemCountRef1, totalRef1)
+        itemCounter2 = new InMemoryItemCounter[F](orderItemCountRef2, totalRef2)
         e1 = OrderCreated("1", NonEmptyList.of(OrderItem("foo")))
         e2 = OrderPaid("1")
-        result1 <- run(itemCounter)(e1) >> run(itemCounter)(e2) >> itemCounter.getCount
-        result2 <- run(itemCounter)(e2) >> run(itemCounter)(e1) >> itemCounter.getCount
+        run1 = run(itemCounter1)
+        run2 = run(itemCounter2)
+        result1 <- run1(e1) >> run1(e2) >> itemCounter1.getCount
+        result2 <- run2(e2).handleError(_ => ()) >> run2(e1) >> itemCounter2.getCount
         _ <- F.delay(println(result1))
         _ <- F.delay(println(result2))
       } yield ()
